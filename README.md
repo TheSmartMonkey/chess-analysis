e64889286a3aa8f180097a3473a5d3db39b04790
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2022-02-12 12:20:26 +0100

    create games.json file

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ Analyse your own games get some good insights that helps you to improve at chess
 
 **Only lichess** data for now
 
+## Getting started
+
+1. Go to https://lichess.org/@/{your_name}/download
+
+1. Download you opening file with this include settings
+
+![APP IMAGE](https://github.com/TheSmartMonkey/chess-analysis/blob/main/.github/temtem-app.PNG)
+
 ## Current Functionalities
 
 1. Give the most played lines (then you now were to train)
@@ -13,3 +21,5 @@ Analyse your own games get some good insights that helps you to improve at chess
 1. Analyse the most common mistakes in the opening
 
 1. Give alternative moves to improve your games
+
+1. Machine leaning (https://github.com/mptedesco/python-chess-analysis)
--- init.py
+++ init.py
@@ -2,10 +2,45 @@ import os
 import json
 
 
-pgn_file = [filename for filename in os.listdir('.') if filename.startswith("lichess")][0]
+def init_pgn_file() -> list:
+    pgn_file = [filename for filename in os.listdir(
+        ".") if filename.startswith("lichess")][0]
+    with open(pgn_file, "r") as file:
+        lines = file.read().splitlines()
 
-with open(pgn_file, "r") as file:
-    lines = file.read().splitlines()
+    return __remove_blank_lines(lines)
 
-print(lines[0])
 
+def __remove_blank_lines(lines) -> list:
+    return [line for line in lines if line != ""]
+
+
+def create_games_json_file(lines):
+    opening_name = __get_opening_name(lines)
+    # result = __get_game_result(lines)
+    pgn = __get_pgn(lines)
+
+    data = [{
+            "opening": opening_name[index],
+            # "win": result[index],
+            "pgn": pgn[index]
+            } for index in range(len(pgn))]
+
+    with open("games.json", "w") as file:
+        json.dump(data, file, indent=4, sort_keys=True)
+
+
+def __get_opening_name(lines) -> list:
+    return [line.replace("[Opening \"", "").replace("\"]", "") for line in lines if line[:8] == "[Opening"]
+
+# def __get_game_result(lines) -> bool:
+#     pass
+
+
+def __get_pgn(lines) -> list:
+    return [line for line in lines if line[0] == "1"]
+
+
+if __name__ == "__main__":
+    lines = init_pgn_file()
+    create_games_json_file(lines)
