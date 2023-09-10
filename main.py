3df923f8eb62214bc5af802d70a9f0554832536f
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2023-09-10 18:13:14 +0200

    move code to new template

--- main.py
+++ main.py
@@ -1,12 +1,51 @@
-from src.functions.hello import http_message, inc, message
+import json
+
+from src.functions.data import init_pgn_file
 
 
-def main() -> None:
-    message()
-    value = inc(3)
-    print(value)
-    print(http_message("This is a test message").message)
+def create_games_json_file(pgn_data: list):
+    opening_name = __get_opening_name(pgn_data)
+    result = __get_game_result(pgn_data)
+    pgn = __get_pgn(pgn_data)
+
+    data = [
+        {"opening": opening_name[index], "result": result[index], "pgn": pgn[index]}
+        for index in range(len(pgn))
+    ]
+
+    with open("games.json", "w") as file:
+        json.dump(data, file, indent=4, sort_keys=True)
+
+
+def __get_opening_name(pgn_data: list) -> list:
+    return [
+        line.replace('[Opening "', "").replace('"]', "")
+        for line in pgn_data
+        if line[:8] == "[Opening"
+    ]
+
+
+def __get_game_result(pgn_data: list) -> bool:
+    data = []
+    for line in pgn_data:
+        if line[:2] == "1.":
+            pgn_split = line.split()
+            result = pgn_split[-1]
+            if result == "1-0":
+                data.append("win")
+            elif result == "1/2-1/2":
+                data.append("equal")
+            else:
+                data.append("lose")
+
+    return data
+
+
+def __get_pgn(pgn_data: list) -> list:
+    return [line for line in pgn_data if line[0] == "1"]
 
 
 if __name__ == "__main__":
-    main()
+    pgn_data = init_pgn_file()
+    print("pgn_data: ", pgn_data)
+    create_games_json_file(pgn_data)
